d182fbc546f9dc8136f17b28f054dc33c1adaaa9
MeikinBR <you@example.com>
2020-10-15 00:07:26 -0300

    Adicionando cabeçalho da licença

--- grecaptchabypass/cache/__init__.py
+++ grecaptchabypass/cache/__init__.py
@@ -1,5 +1,14 @@
 from typing import Tuple
 
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                      Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+"""
+
 
 class ReCAPTCHACache:
     _cache = []
